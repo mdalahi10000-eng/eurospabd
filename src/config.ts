178a48021d3b2ca86d0ea8899d4e22633f751b9f
mdalahi10000-eng <mdalahi10000@gmail.com>
2026-08-06 12:55:40 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ export const WHATSAPP_NUMBER = '8801842658423';
 
 export const PHONE_NUMBER = '01842-658423';
 
-export const ADDRESS = '1 Road 8, Banani, Dhaka 1212, Bangladesh';
+export const ADDRESS = '7 Kemal Ataturk Ave, Dhaka 1212, Bangladesh';
 
 export const GOOGLE_MAPS_EMBED =
-  'https://www.google.com/maps?q=Euro+Spa+Center,+1+Road+8,+Banani,+Dhaka+1212,+Bangladesh&output=embed';
+'https://www.google.com/maps?q=Euro+Spa+Center,+7+Kemal+Ataturk+Ave,+Dhaka+1212,+Bangladesh&output=embed';
 
 export const BUSINESS_HOURS = [
   { day: 'Monday – Friday', hours: '9:00 AM – 9:00 PM' },
